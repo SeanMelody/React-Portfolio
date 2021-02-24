dbe19df0231cd35ed01079ba32a0e3a06e048c72
Sean Melody <seandmelody@gmail.com>
2021-02-24 15:15:42 -0800

    Basic Home, Portfolio, Contact Pages Added and connected

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Nav from "./Components/Nav/Nav"
+import Home from "./Pages/Home/Home"
+import Portfolio from "./Pages/Portfolio/Portfolio"
+import Contact from "./Pages/Contact/Contact"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-
+        <Switch>
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,11 +5,12 @@ import styles from "./NavStyles"
 function Nav() {
     return (
         <nav style={styles.navStyles}>
+            <h1>Sean Melody</h1>
             <Link to="/" style={styles.linkStyles}>
                 <p>Home</p>
             </Link>
-            <Link to="/about" style={styles.linkStyles}>
-                <p>About</p>
+            <Link to="/portfolio" style={styles.linkStyles}>
+                <p>Portfolio</p>
             </Link>
             <Link to="/contact" style={styles.linkStyles}>
                 <p>Contact</p>
