caad6b8e2f0667f84e4fc25d5333cc9399cbfd28
Sean Melody <seandmelody@gmail.com>
2021-02-25 11:58:24 -0800

    third row added to portfolio

--- src/Components/PortfolioSection/PortfolioSection.js
+++ src/Components/PortfolioSection/PortfolioSection.js
@@ -3,17 +3,17 @@ import React from 'react'
 const PortfolioSection = (props) => {
     return (
 
-        <div class="col-md-4 portfolio-img text-center">
-            <img src={props.photo} alt={props.project} class="img-thumbnail"></img>
-            <div class="row">
-                <div class="col-md-12 social-media-link">
+        <div className="col-md-4 portfolio-img text-center">
+            <img src={props.photo} alt={props.project} className="img-thumbnail"></img>
+            <div className="row">
+                <div className="col-md-12 social-media-link">
                     <a href={props.link} alt={props.project} target="_blank" rel="noreferrer">
                         <h4>{props.project}</h4>
                     </a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 social-media-link">
+            <div className="row">
+                <div className="col-md-12 social-media-link">
                     <a href={props.github} alt="github" target="_blank" rel="noreferrer">
                         <h4>GitHub Link</h4>
                     </a>
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from "../../Components/Header/Header"
 import PortfolioSection from "../../Components/PortfolioSection/PortfolioSection"
 
-const Portfolio = (props) => {
+const Portfolio = () => {
     return (
         <div>
             <Header title={"Portfolio"} />
-            <div class="row portfolio-body">
+            <div className="row portfolio-body">
                 <PortfolioSection
                     project={"Employee Database"}
                     photo={"./images/Employee-Directory.png"}
@@ -27,7 +27,7 @@ const Portfolio = (props) => {
                 />
             </div>
             <br></br>
-            <div class="row portfolio-body">
+            <div className="row portfolio-body">
                 <PortfolioSection
                     project={"API Password Generator"}
                     photo={"./images/API-Password-Generator.png"}
@@ -47,7 +47,27 @@ const Portfolio = (props) => {
                     github={"https://github.com/SeanMelody/Weather-API"}
                 />
             </div>
-
+            <br></br>
+            <div className="row portfolio-body">
+                <PortfolioSection
+                    project={"Day Scheduler"}
+                    photo={"./images/Scheduler.png"}
+                    link={"https://seanmelody.github.io/Scheduler/"}
+                    github={"https://github.com/SeanMelody/BurgerEater"}
+                />
+                <PortfolioSection
+                    project={"JavaScript Coding Quiz"}
+                    photo={"./images/JavaScript-Coding-Quiz.png"}
+                    link={"https://seanmelody.github.io/JavaScript-Coding-Quiz/"}
+                    github={"https://github.com/SeanMelody/JavaScript-Coding-Quiz"}
+                />
+                <PortfolioSection
+                    project={"Selfies With Arya"}
+                    photo={"./images/Selfies-With-Arya.png"}
+                    link={"https://seanmelody.github.io/Arya/"}
+                    github={"https://github.com/SeanMelody/Arya"}
+                />
+            </div>
 
 
         </div>
