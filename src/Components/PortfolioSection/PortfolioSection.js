34c10028be415999b794eba683a7a153f747628c
Sean Melody <seandmelody@gmail.com>
2021-02-25 11:19:03 -0800

    Day Planner Added to Portfolio

--- src/Components/PortfolioSection/PortfolioSection.js
+++ src/Components/PortfolioSection/PortfolioSection.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const PortfolioSection = (props) => {
     return (
-        <div>
-            <div class="col-md-4 portfolio-img text-center">
-                <img src={props.photo} alt={props.project} class="img-thumbnail"></img>
-                <div class="row">
-                    <div class="col-md-12 social-media-link">
-                        <a href={props.link} alt={props.project} target="_blank" rel="noreferrer">
-                            <h4>{props.project}</h4>
-                        </a>
-                    </div>
+
+        <div class="col-md-4 portfolio-img text-center">
+            <img src={props.photo} alt={props.project} class="img-thumbnail"></img>
+            <div class="row">
+                <div class="col-md-12 social-media-link">
+                    <a href={props.link} alt={props.project} target="_blank" rel="noreferrer">
+                        <h4>{props.project}</h4>
+                    </a>
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -7,10 +7,14 @@ const Portfolio = (props) => {
         <div>
             <Header title={"Portfolio"} />
             <div class="row portfolio-body">
-
                 <PortfolioSection
                     project={"Employee Database"}
                     photo={"./images/Employee-Directory.png"}
+                    link={"https://secret-escarpment-97413.herokuapp.com/"}
+                />
+                <PortfolioSection
+                    project={"Day Planner"}
+                    photo={"./images/Day-Planner.png"}
                     link={"https://secret-escarpment-97413.herokuapp.com/"}
                 />
 
