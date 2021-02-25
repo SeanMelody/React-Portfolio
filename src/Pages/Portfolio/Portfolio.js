27bdb5de5441e3bc622633436f085475cb6173f9
Sean Melody <seandmelody@gmail.com>
2021-02-25 11:27:13 -0800

    Additional Projects added to Portfolio

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -17,8 +17,32 @@ const Portfolio = (props) => {
                     photo={"./images/Day-Planner.png"}
                     link={"https://secret-escarpment-97413.herokuapp.com/"}
                 />
-
+                <PortfolioSection
+                    project={"Burger Eater"}
+                    photo={"./images/Burger-Eater.png"}
+                    link={"https://limitless-reaches-92941.herokuapp.com/"}
+                />
             </div>
+            <div class="row portfolio-body">
+                <PortfolioSection
+                    project={"API Password Generator"}
+                    photo={"./images/API-Password-Generator.png"}
+                    link={"https://seanmelody.github.io/PWDGenerator/"}
+                />
+                <PortfolioSection
+                    project={"Offline/Online Budget Tracker"}
+                    photo={"./images/Budget-Tracker.png"}
+                    link={"https://shrouded-bayou-14667.herokuapp.com/"}
+                />
+                <PortfolioSection
+                    project={"React Employee Directory"}
+                    photo={"./images/Employee-Directory.png"}
+                    link={"https://employee-directory-sm.herokuapp.com/"}
+                />
+            </div>
+
+
+
         </div>
 
     )
